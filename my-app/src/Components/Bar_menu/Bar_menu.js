a460201a4df83aef129e55efcd6a1e253637abf5
Fabien <84633381+Zearcus@users.noreply.github.com>
2022-06-28 21:14:14 +0200

    modif

--- my-app/src/Components/Bar_menu/Bar_menu.js
+++ my-app/src/Components/Bar_menu/Bar_menu.js
@@ -8,21 +8,14 @@ class Bar_menu extends Component {
   render() {
     return (
         
-      <Navbar className="No shadow barMenu" expand="lg" variant="light" bg="light">
-          <Row>
-            {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse> */}
-            <Col sm md lg>
-              <Nav className="navBar">
-                <Navbar.Brand className="Title">Mapapa</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Link to="/Admin">Yeti Smash<Badge className="Blink" pill bg="danger">New</Badge></Link>
-                </Navbar.Collapse>
-                
-              </Nav>
-            </Col>
-          </Row>
+      <Navbar className="shadow barMenu" expand="lg"  bg="light">
+          <Navbar.Brand className="Title">Mapapa</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav ">
+            <Nav className="navBar">
+              <Link to="/Admin">Yeti Smash<Badge className="Blink" pill bg="danger">New</Badge></Link>
+            </Nav>
+          </Navbar.Collapse>
       </Navbar>
        
     )
